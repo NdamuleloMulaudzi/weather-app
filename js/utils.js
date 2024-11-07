3a659f1ea00f61a274d0739ce359db8ea5815ca9
NdamuleloMulaudzi <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2024-11-07 13:30:23 +0200

    logic for requesting an API

---
+++ js/utils.js
@@ -0,0 +1,13 @@
+
+async function getWeatherData(locationName){
+   const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationName}?key=T3NEGCXRXBKQ8WV3NRQY2LWPC`, 
+    {mode:"cors"})
+   
+    const weatherData = await response.json()
+    
+    console.log(weatherData)
+}
+
+getWeatherData("London")
+
+export {getWeatherData}
